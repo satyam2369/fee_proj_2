e7c4921b69df67ed435c6f6166013af1538d13c9
satyam2369 <102215552+satyam2369@users.noreply.github.com>
2023-08-27 15:09:26 +0530

    Add files via upload

---
+++ game_logic.js
@@ -0,0 +1,69 @@
+let playerText=document.getElementById('playerText')
+let restartBtn=document.getElementById('restartBtn')
+let boxes=Array.from(document.getElementsByClassName('box'))
+
+let winningIndicator=getComputedStyle(document.body).getPropertyValue('--winning-blocks')
+console.log(boxes);
+
+const O_TEXT="O"
+const X_TEXT="X"
+let currentPlayer=X_TEXT
+let spaces=Array(9).fill(null)
+
+const startGame=()=>{
+    boxes.forEach(box=> box.addEventListener('click',boxClicked))
+}
+function boxClicked(e){
+    console.log(e.target);
+    const id=e.target.id
+
+    if(!spaces[id]){
+        spaces[id]=currentPlayer
+        e.target.innerText=currentPlayer
+        if(playerHasWon()!=false){
+            playerText.innerText=`${currentPlayer} Has Won!!!`
+            let winning_blocks=playerHasWon();
+
+            // console.log(winning_blocks);
+            winning_blocks.map(box=> boxes[box].style.backgroundColor=winningIndicator)
+            boxes.forEach(box=> box.removeEventListener('click',boxClicked))
+            return;
+        }
+
+        currentPlayer=currentPlayer==X_TEXT?O_TEXT:X_TEXT;
+    }
+}
+const winningCombos=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [3,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
+function playerHasWon(){
+    for (const condition of winningCombos) {
+        let[a,b,c]=condition
+
+        if(spaces[a]&&(spaces[a]==spaces[b] && spaces[a]==spaces[c])){
+            return [a,b,c]
+        }
+    }
+    return false
+}
+restartBtn.addEventListener('click',restart)
+function restart(){
+    spaces.fill(null)
+
+    boxes.forEach(box =>{
+        box.innerText=''
+        box.style.backgroundColor=''
+    })
+    playerText.innerText='Tic Tac Toe'
+    currentPlayer=X_TEXT
+    boxes.forEach(box=> box.addEventListener('click',boxClicked))
+}
+startGame()
+
